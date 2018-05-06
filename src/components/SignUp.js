515abf83d071b529192910dce794df5c7cbd9c9d
Kyle Kurtz <kjkurtz@gmail.com>
2018-05-05 21:33:35 -0500

    Added login

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -44,6 +44,7 @@ class SignUpForm extends Component {
     auth.doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
         this.setState(() => ({ ...INITIAL_STATE}));
+        history.push(routes.HOME);
       })
       .catch(error => {
         this.setState(byPropKey('error', error));
